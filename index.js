c8bcf57374c821f28c14cc1d1cae78ea841acaa5
md ashik mia <halima520ashu@gmail.com>
2026-06-27 16:32:24 +0600

    added adim role fetaure

--- index.js
+++ index.js
@@ -27,7 +27,10 @@ async function run() {
     const database = client.db("yong-man");
     const userCollection = database.collection("user");
     const classCollection = database.collection("all-class");
+    const favoriteClassCollection = database.collection("favorite");
     const paymentCollection = database.collection("payment");
+    const forumCollection = database.collection("forum");
+    const trainerFormCollection = database.collection("trainerForm");
 
     app.get("/", async (req, res) => {
       const result = await userCollection.find().toArray();
@@ -44,13 +47,18 @@ async function run() {
       const allClass = await classCollection.find().toArray();
       res.status(200).send(allClass);
     });
-    
 
+    // get user created class by id
 
+    app.get("/api/myClass", async (req, res) => {
+      const query = {};
+      if (req.query.userId) {
+        query.userId = req.query.userId;
+      }
 
-
-
-
+      const result = await classCollection.find(query).toArray();
+      res.send(result);
+    });
 
     // get bu query
     app.get("/api/allClass/:id", async (req, res) => {
@@ -72,6 +80,65 @@ async function run() {
       const result = await paymentCollection.insertOne(data);
       res.status(201).send(result);
     });
+    app.get("/api/payment", async (req, res) => {
+      const query = {};
+      if (req.query.userId) {
+        query.userId = req.query.userId;
+      }
+      const result = await paymentCollection.find(query).toArray();
+      res.send(result);
+    });
+    // community forum related api
+    app.post("/api/forum", async (req, res) => {
+      const forum = req.body;
+      const createAt = new Date();
+      const postData = { ...forum, createAt };
+      const newForum = await forumCollection.insertOne(postData);
+      res.status(201).send(newForum);
+    });
+    app.get("/api/forum", async (req, res) => {
+      const query = {};
+      if (req.query.userId) {
+        query.userId = req.query.userId;
+      }
+      const result = await forumCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/api/forum/:id", async (req, res) => {
+      const id = req.params;
+      const query = { _id: new ObjectId(id) };
+      const result = await forumCollection.findOne(query);
+      res.send(result);
+    });
+    // apply as trainer related api
+    app.post("/api/applyAsTrainer",async(req,res)=>{
+      const data=req.body;
+      const createAt=new Date();
+      const trainerForm={...data,createAt}
+      const result= await trainerFormCollection.insertOne(trainerForm)
+      res.status(201).send(result)
+    })
+    app.get("/api/applyAsTrainer",async(req,res)=>{
+      const result =await trainerFormCollection.find().toArray()
+      res.send(result)
+    })
+
+    // favorite related api
+    app.post("/api/favorite",async(req,res)=>{
+      const data=req.body;
+      const result= await favoriteClassCollection.insertOne(data);
+      res.status(201).send(result)
+
+    })
+     app.get("/api/favorite",async(req,res)=>{
+      const query={}
+      if(req.query.userId){
+        query.userId=req.query.userId
+      }
+      const result= await favoriteClassCollection.find(query).toArray()
+      res.send(result)
+
+    })
 
     await client.db("admin").command({ ping: 1 });
 
